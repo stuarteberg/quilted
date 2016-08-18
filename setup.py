dbd735ed86c3e5a4fe9111cb7fd173bfeffdd03e
Stuart Berg <bergs@janelia.hhmi.org>
2016-08-18 17:07:14 -0400

    Added setup.py, license, readme, and conda-recipe

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+os.path.split(__file__)[0] + '/README'
+
+with open('README') as readme:
+    readme_contents = readme.read()
+
+setup(
+    name='quilted',
+    version='0.1',
+    long_description=readme_contents,
+    packages=['quilted'],
+    include_package_data=True,
+    zip_safe=False,
+    #install_requires=['numpy, h5py']
+)
